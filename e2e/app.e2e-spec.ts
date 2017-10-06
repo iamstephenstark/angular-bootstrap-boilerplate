7d28e475937ce53e4b3f656b2037d136b6ee5268
Stephen J. Stark <sstark@mecnet.net>
2017-10-06 08:12:42 -0400

    Fixed e2e test with correct welcome message

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-bootstrap-boilerplate App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to angular-bootstrap-boilerplate!');
   });
 });
